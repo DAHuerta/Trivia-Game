0878a629a4649976da1462dd801ef30f1a070e0c
Daniel Huerta <danielhuerta@Daniels-MacBook-Pro.local>
2019-09-14 11:09:07 -0600

    added some questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,45 +7,45 @@ var timerRunning = "";
 var questions = [
 
     {
-        question: qqa,
-        answerOptions: [ada],
+        question: "What is Stan’s dad (Randy) do for a living?",
+        answerOptions: ["Architect", "Professional Mascot", "Geologiest", "Meteorologist"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What does Towelie like to do?",
+        answerOptions: ["Drink Windex", "Get High", "Play Frisbee", "Smoke Meth"],
         correctAnswer: 1
     },
 
     {
-        question: qqa,
-        answerOptions: [ada],
+        question: "Why did Cartman's friends ignore him in the “death of Eric Cartman” episode?",
+        answerOptions: ["He ate all the skin of the KFC Chicken", "He is an asshole", "He took credit for the 'Fish Dicks' joke", "He called Kyle a Jew"],
+        correctAnswer: 0
+    },
+
+    {
+        question: "What is the name of the South Park Elementary School counselor?",
+        answerOptions: ["Mr. Garrison", "Mr. Mackey", "Mr. Marsh", "Mr. Hat"],
         correctAnswer: 1
     },
 
     {
-        question: qqa,
-        answerOptions: [ada],
-        correctAnswer: 1
+        question: "Why did Issac Hayes and his character (Chef) leave the show?",
+        answerOptions: ["He retired", "The show was to offensive", "The show decided to go on without him", "The creators made fun of Scientology"],
+        correctAnswer: 3
     },
 
     {
-        question: qqa,
-        answerOptions: [ada],
-        correctAnswer: 1
+        question: "What is the name of Cartman’s christan boy band?",
+        answerOptions: ["Heavenly Soldiers", "Hands on Bible", "Faith Plus 1", "Sinners"],
+        correctAnswer: 2
     },
 
     {
-        question: qqa,
-        answerOptions: [ada],
-        correctAnswer: 1
-    },
-
-    {
-        question: qqa,
-        answerOptions: [ada],
-        correctAnswer: 1
-    },
-
-    {
-        question: qqa,
-        answerOptions: [ada],
-        correctAnswer: 1
+        question: "What is a recurring event in almost all South Park Episodes?",
+        answerOptions: ["Kenny Dies", "Cartman eat KFC chicken", "Randy does something stupid", "ManBearPig mes an apperance"],
+        correctAnswer: 0
     },
 
     {
