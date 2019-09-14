9cd9949809fd4ebc983605f43456d5e3dac0f65d
Daniel Huerta <danielhuerta@Daniels-MacBook-Pro.local>
2019-09-14 10:43:42 -0600

    HTML skeleton and start javascript questions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var timer = 30;
+var timerRunning = "";
+
+var questions = [
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+
+    {
+        question: qqa,
+        answerOptions: [ada],
+        correctAnswer: 1
+    },
+]
